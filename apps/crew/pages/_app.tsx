e35af55480770aacca12c5143652d80545ca85c7
Phillip DaCosta <phillip.dacosta@gmail.com>
2024-06-25 11:36:46 -0400

    updates

--- apps/crew/pages/_app.tsx
+++ apps/crew/pages/_app.tsx
@@ -7,7 +7,7 @@ function CustomApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <title>Welcome to crew!</title>
+        <title>Welcome to crew test!</title>
       </Head>
       <div className="app">
         <header className="flex">
